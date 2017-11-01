77fe6063fae316c34898b5efa3d184cf61068639
Brent Edwards <bedwards@liquidinteractive.com.au>
2017-11-01 14:42:54 +1000

    Initial Commit

---
+++ DreamworldHomepage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DreamworldHomepage.Startup))]
+namespace DreamworldHomepage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
